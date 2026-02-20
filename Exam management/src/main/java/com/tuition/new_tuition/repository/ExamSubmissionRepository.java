6c104b19db227211f3d52c39517dc5cfd905bf74
IT24101204 <it24101204@my.sliit.lk>
2026-02-20 10:35:14 +0530

    Create ExamSubmissionRepository.java

---
+++ Exam management/src/main/java/com/tuition/new_tuition/repository/ExamSubmissionRepository.java
@@ -0,0 +1,6 @@
+package com.tuition.new_tuition.repository;
+
+import com.tuition.new_tuition.entity.ExamSubmission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamSubmissionRepository extends JpaRepository<ExamSubmission, Long> {}
